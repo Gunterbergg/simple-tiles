d14b41181f2c9fcfdf15c3f822e3395a1074796e
GustavoRossi <gustavo.rocha@lumetec.com.br>
2020-11-06 18:03:09 -0300

    Added prototype of tile rendering system
    Initialized tiles array in tileLines objects;
    Added songTick() to keep track of tiles;
    Added config to set tile minimum size;
    Tile minimum size set to 30;
    
    Tick() now tracks a counter variable;
    Tick step set to 1;
    
    Renamed keyboards to keyboard, it's not an array anymore;
    Changed loop to update only one keyboard;
    Changed loop-interval to 10;

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 const canvas = document.querySelector("#drawer");
-const keyboards = [];
+let keyboard;
 
 const config = 
 {
-	loopInterval: 6,
+	loopInterval: 10,
 	canvasSize: Math.min(window.innerHeight, window.innerWidth),
 	keyFont: "12px Verdana",
 	keyColor: "#ffffff",
 	background: "#ffffff",
 	keyTilePercentSize: 11,
+	minTileSize: 30
 }
 
 document.addEventListener("DOMContentLoaded", main);
@@ -24,6 +25,9 @@ function Keyboard(canvas, vecPos, vecSize, tileLines)
 	this.vecPos = vecPos;
 	this.vecSize = vecSize;
 	this.tileLines = tileLines;
+	this.tickStep = 1;
+	this.tickCount = 0;
+	this.playingMusic = null;
 	document.addEventListener("keypress", (event) => this.onKeyPress(event));
 	//document.addEventListener("keydown", (event) => this.onKeyDown(event));
 	document.addEventListener("keyup", (event) => this.onKeyUp(event));
@@ -32,11 +36,15 @@ function Keyboard(canvas, vecPos, vecSize, tileLines)
 Keyboard.prototype.tick = function()
 {
 	this.draw();
+	this.songTick();
+	this.tickCount += this.tickStep;
 };
 
 Keyboard.prototype.draw = function()
 {
  	const canvas = this.canvas;
+
+ 	//Clear canvas
  	canvas.fillStyle = config.background;
  	canvas.fillRect(this.vecPos.x, this.vecPos.y, this.vecSize.x, this.vecSize.y);
  	//canvas.clearRect(this.vecPos.x, this.vecPos.y, this.vecSize.x, this.vecSize.y);
@@ -49,6 +57,12 @@ Keyboard.prototype.draw = function()
 	for(let i = 0, length1 = this.tileLines.length; i < length1; i++){
 		const tileLine = this.tileLines[i];
 		const keyTileXPos = i * tileLineWidth;
+
+		//Draw tiles
+		canvas.fillStyle = tileLine.color;
+		for (let tile of tileLine.tiles) {
+			canvas.fillRect(keyTileXPos, this.tickCount - tile.t - tile.l, tileLineWidth, tile.l);
+		}
 
 		//Draw keyTiles
 		canvas.fillStyle = tileLine.color;
@@ -68,7 +82,7 @@ Keyboard.prototype.draw = function()
 		if (tileLine.isPressed) {
 			const pressedGradient = canvas.createLinearGradient(keyTileXPos + tileLineWidth / 2, this.vecPos.y + this.vecSize.y / 2, keyTileXPos + tileLineWidth / 2, this.vecSize.y - keyTileHeight);
 			pressedGradient.addColorStop(0, config.background + "00");
-			pressedGradient.addColorStop(1, tileLine.color + "b0");
+			pressedGradient.addColorStop(1, tileLine.color + "c0");
 			canvas.fillStyle = pressedGradient;
 			canvas.fillRect(keyTileXPos, this.vecPos.y, tileLineWidth, this.vecSize.y - keyTileHeight);
 		}
@@ -91,26 +105,60 @@ Keyboard.prototype.onKeyUp = function(event)
 	}
 };
 
+
+Keyboard.prototype.songTick = function(music)
+{
+	for(let i = 0; i < this.playingMusic.length; i++){
+		for (const tile of this.playingMusic[i]) {
+			const bottomPos = tile.l - tile.t + this.tickCount;
+			const tilesList = this.tileLines[i].tiles;
+			if (tilesList.includes(tile)) {
+				//TODO: remove tile if too big if (bottomPos > this.vecSize.y) tilesList.remove()
+				continue;
+			}
+			
+			if (bottomPos < 0) continue;
+			tile.l = Math.max(tile.l, config.minTileSize);
+			tilesList.push(tile); 
+		}
+	}
+};
+
+Keyboard.prototype.startSong = function(music)
+{
+	this.playingMusic = music;
+};
+
+
 function main ()
 {
 	canvas.width = config.canvasSize;
 	canvas.height = config.canvasSize;
-	keyboards.push(
-		new Keyboard(canvas, {x:0, y:0}, {x:canvas.width, y:canvas.height},
+	keyboard = new Keyboard(canvas, {x:0, y:0}, {x:canvas.width, y:canvas.height},
 			[ 
-				{ color:"#f03434", key:"a", isPressed:true },
-				{ color:"#22a7f0", key:"s", },
-				{ color:"#2ecc71", key:"d", },
-				{ color:"#f5e653", key:"j", },
-				{ color:"#674172", key:"k", },
-				{ color:"#f9690e", key:"l", },
-			] ));
+				{ color:"#f03434", key:"a", isPressed:true, tiles: [] },
+				{ color:"#22a7f0", key:"s", isPressed:true, tiles: [] },
+				{ color:"#2ecc71", key:"d", isPressed:true, tiles: [] },
+				{ color:"#f5e653", key:"j", isPressed:true, tiles: [] },
+				{ color:"#674172", key:"k", isPressed:true, tiles: [] },
+				{ color:"#f9690e", key:"l", isPressed:true, tiles: [] },
+			]);
+	const music = 
+	[
+		[ { t: 100, l: 0} ],
+		[ { t: 200, l: 0} ],
+		[ { t: 300, l: 0} ],
+		[ { t: 400, l: 0} ],
+		[ { t: 500, l: 0} ],
+		[ { t: 600, l: 0} ]
+	]
+
+	keyboard.startSong(music);
 
 	setInterval(loop, config.loopInterval);
 }
 
 function loop()
 {
-	for (const keyboard of keyboards)
-		keyboard.tick();
+	keyboard.tick();
 }
